c7bd1ad3c283a0bf92e9f5a99573956883c09026
Chad Ross <rosschad98@icloud.com>
2021-07-26 21:46:08 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+//import ReactDOM from 'react-dom';
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const fb = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwbEVuPBBU70vUpspecnHrWnE3k1dOu90",
+  authDomain: "closet-space-22231.firebaseapp.com",
+  databaseURL: "https://closet-space-22231-default-rtdb.firebaseio.com",
+  projectId: "closet-space-22231",
+  storageBucket: "closet-space-22231.appspot.com",
+  messagingSenderId: "93935836929",
+  appId: "1:93935836929:web:e14a7be76ce346b48d4500",
+  measurementId: "G-L4L6VHW6W5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+function ClosetSpace() {
+  return(
+    <div>
+      <p>Hello World!</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ClosetSpace />
       </header>
     </div>
   );
