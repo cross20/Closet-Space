1b50043dedab0c7e218c7c07e02040ded04b7255
Chad Ross <rosschad98@icloud.com>
2021-09-18 11:05:28 -0500

    Fix bug which prevented state from being updated properly. Each action (save, delete, update, etc.) now handles setting savedItems and editRowId. Modes are no longer used

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-import React, {useState, Fragment} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import { nanoid } from 'nanoid';
 import data from "./mock-items";
-//import ReactDOM from 'react-dom';
-
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-import "firebase/auth";
-
-// eslint-disable-next-line
-const fb = require("firebase"); // TODO: implement database.
-// Required for side-effects
-require("firebase/firestore");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAwbEVuPBBU70vUpspecnHrWnE3k1dOu90",
-  authDomain: "closet-space-22231.firebaseapp.com",
-  databaseURL: "https://closet-space-22231-default-rtdb.firebaseio.com",
-  projectId: "closet-space-22231",
-  storageBucket: "closet-space-22231.appspot.com",
-  messagingSenderId: "93935836929",
-  appId: "1:93935836929:web:e14a7be76ce346b48d4500",
-  measurementId: "G-L4L6VHW6W5"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// eslint-disable-next-line
-const db = firebase.firestore(); // TODO: implement database
 
 /**
  * Table row which displays an item.
  * @param {*[]} item Object displayed in the table.
- * @param {*} disabled True of a different row is being edited. Otherwise false.
+ * @param {*} disabled True if a different row is being edited. Otherwise false.
  * @param {*} onEdit Method which sets the id of the row being edited.
  */
 const ReadOnlyRow = ({item, disabled, onEdit}) => {
@@ -55,17 +29,27 @@ const ReadOnlyRow = ({item, disabled, onEdit}) => {
  * @param {*} onChange Method which modifies the values of the the item.
  * @param {*} onCancel Method which responds to the Cancel button being selected.
  */
-const EditRow = ({item, onChange, onCancel}) => {
+const EditRow = ({item, onChange, onDelete, onCancel}) => {
+
+  const Actions = () => {
+    return (
+      <div>
+        <button onClick={onCancel}>
+          Cancel
+        </button>
+        <button onClick={(item) => onDelete(item.id)}>
+          Delete
+        </button>
+      </div>
+    );
+  }
+
   return (
     <ItemRow item={item}
       onChange={(newItem) => onChange(newItem)}
       disabled={false}
       submitValue="Save" 
-      actions={
-        <button onClick={onCancel}>
-          Cancel
-        </button>
-      }>
+      actions={<Actions />}>
     </ItemRow>
   );
 }
@@ -96,8 +80,16 @@ const NewRow = ({newItem, disabled, onChange}) => {
  */
 const ItemRow = ({item, disabled, onChange, submitValue, actions}) => {
 
-  if (actions === undefined) {
-    actions = () => {return (<div />)}
+  const Actions = () => {
+    if (actions === undefined) {
+      return (<div />);
+    }
+
+    return (
+      <div>
+        {actions}
+      </div>
+    );
   }
 
   const changeItem = (event) => {
@@ -151,7 +143,9 @@ const ItemRow = ({item, disabled, onChange, submitValue, actions}) => {
         disabled={disabled}
         value={submitValue}>
       </input>
-      <Fragment>{actions}</Fragment>
+      <Fragment>
+        <Actions />
+      </Fragment>
     </td>
   </tr>
   );
@@ -162,31 +156,57 @@ const ItemRow = ({item, disabled, onChange, submitValue, actions}) => {
  */
 const Items = () => {
 
+  /** Item containing all the default values and a random ID. */
   const defaultItem = {
-      id: nanoid(),
-      name: "",
-      condition: "",
-      quantity: 0
-    };
+    id: nanoid(),
+    name: "",
+    condition: "",
+    quantity: 0
+  };
 
-  const [items, setItems] = useState(data);
-  const [displayItems, setDisplayItems] = useState(items);
+  /** Set of items which have been saved by the user. These do not reflect what the user sees until the user adds or saves changes to an item. */
+  const [savedItems, setItems] = useState(data);
+  /** Set of items displayed to the user. These reflect saved and unsaved changes to the set of items. */
+  const [displayItems, setDisplayItems] = useState(savedItems);
+  /** Item which can be added to the set of existing items. */
   const [newItem, setNewItem] = useState(defaultItem);
+  /** ID representing the item being edited. Default is -1. */
   const [editRowId, setEditRowId] = useState(-1);
 
+  /**
+   * Add a new item if the row ID is -1. Otherwise, save changes to an existing item.
+   * @param {*} event 
+   */
   const updateItems = (event) => {
     event.preventDefault();
 
+    // Add a new row unless we're editing an existing row.
     if (editRowId === -1) {
       appendDisplayItems(newItem);
       setNewItem(defaultItem);
-    } else {
+    } else if (validateRowId(editRowId)) {
+      setItems(displayItems);
       setEditRowId(-1);
     }
-
-    setItems(displayItems);
   }
 
+  /**
+   * Add a new item.
+   * @param {*} newItem Item to add.
+   */
+  const appendDisplayItems = (newItem) => {
+    const newItems = [ ...displayItems ];
+
+    newItems.push(newItem);
+
+    setDisplayItems(newItems);
+    setItems(newItems);
+  }
+
+  /**
+   * Changes an existing item.
+   * @param {*} newItem Replaces the existing item.
+   */
   const updateDisplayItems = (newItem) => {
     const newItems = [ ...displayItems ];
 
@@ -197,19 +217,45 @@ const Items = () => {
     }
 
     setDisplayItems(newItems);
+    // Do not set items here because we haven't saved our changes yet.
   }
 
+  /**
+   * Cancel changes to an existing item by overwriting all display items with saved items.
+   */
   const revertDisplayItems = () => {
-    setDisplayItems(items);
+    setDisplayItems(savedItems);
     setEditRowId(-1);
   }
 
-  const appendDisplayItems = (newItem) => {
+  /**
+   * Prompt the user to confirm they would like to delete an item. If confirmed, delete the item.
+   * @param {*} oldItemId ID of the item to delete.
+   */
+  const deleteDisplayItems = (oldItemId) => {
     const newItems = [ ...displayItems ];
+    const oldItemIndex = newItems.findIndex(item => item.id === oldItemId);
+    newItems.splice(oldItemIndex, 1);
 
-    newItems.push(newItem);
+    if (window.confirm("Delte this item?")) {
+      setDisplayItems(newItems);
+      setItems(newItems);
+      setEditRowId(-1);
+    }
+  }
 
-    setDisplayItems(newItems);
+  /**
+   * Verifies a row with the provided ID exists in the display items.
+   * @param {*} rowId ID corresponding to an item.
+   */
+  const validateRowId = (rowId) => {
+    if (displayItems.findIndex(item => item.id === rowId) > -1) {
+      return true;
+    }
+
+    console.log("Could not validate row ID: ", rowId);
+
+    return false;
   }
 
   return (
@@ -227,12 +273,26 @@ const Items = () => {
         <tbody>
           {displayItems.map((item) => (
             <Fragment key={item.id}>
-              { item.id === editRowId ?
-                <EditRow key={item.id} item={item} onChange={(newItem) => updateDisplayItems(newItem)} onCancel={() => revertDisplayItems()}></EditRow> :
-                <ReadOnlyRow key={item.id} item={item} disabled={editRowId !== -1} onEdit={(rowId) => setEditRowId(rowId)}></ReadOnlyRow> }
+              {item.id === editRowId ?
+                <EditRow key={item.id} item={item}
+                  onChange={(newItem) => updateDisplayItems(newItem)}
+                  onDelete={(oldItemId) => deleteDisplayItems(oldItemId)}
+                  onCancel={() => revertDisplayItems()}>
+                </EditRow>
+              :
+                <ReadOnlyRow key={item.id} 
+                  item={item} 
+                  disabled={editRowId !== -1} 
+                  onEdit={(rowId) => setEditRowId(rowId)}>
+                </ReadOnlyRow>
+              }
             </Fragment>
           ))}
-          <NewRow id={newItem.id} disabled={editRowId !== -1} newItem={newItem} onChange={(newItem) => setNewItem(newItem)}></NewRow>
+          <NewRow id={newItem.id}
+            disabled={editRowId !== -1}
+            newItem={newItem}
+            onChange={(newItem) => setNewItem(newItem)}>
+          </NewRow>
         </tbody>
       </table>
     </div>
